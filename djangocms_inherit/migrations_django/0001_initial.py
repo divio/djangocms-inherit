ec237e8fcd103609f1dba1c60f9acf090ef3debe
Iacopo Spalletti <i.spalletti@nephila.it>
2014-08-16 14:05:23 +0200

    Recreate migrations

--- djangocms_inherit/migrations_django/0001_initial.py
+++ djangocms_inherit/migrations_django/0001_initial.py
@@ -7,16 +7,16 @@ from django.db import models, migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('cms', '__first__'),
+        ('cms', '0001_initial'),
     ]
 
     operations = [
         migrations.CreateModel(
             name='InheritPagePlaceholder',
             fields=[
-                ('from_language', models.CharField(choices=[(b'en', b'English'), (b'de', b'German'), (b'fr', b'French'), (b'pt-br', b'Brazilian Portuguese'), (b'es-mx', 'Espa\xf1ol')], max_length=5, blank=True, help_text='Optional: the language of the plugins you want', null=True, verbose_name='language')),
-                ('cmsplugin_ptr', models.OneToOneField(auto_created=True, primary_key=True, serialize=False, to='cms.CMSPlugin')),
-                ('from_page', models.ForeignKey(blank=True, to='cms.Page', help_text='Choose a page to include its plugins into this placeholder, empty will choose current page', null=True)),
+                ('cmsplugin_ptr', models.OneToOneField(serialize=False, parent_link=True, auto_created=True, to='cms.CMSPlugin', primary_key=True)),
+                ('from_language', models.CharField(help_text='Optional: the language of the plugins you want', blank=True, max_length=5, choices=[('en', 'English'), ('de', 'German'), ('fr', 'French'), ('pt-br', 'Brazilian Portuguese'), ('es-mx', 'Español')], verbose_name='language', null=True)),
+                ('from_page', models.ForeignKey(help_text='Choose a page to include its plugins into this placeholder, empty will choose current page', blank=True, to='cms.Page', null=True)),
             ],
             options={
                 'abstract': False,
